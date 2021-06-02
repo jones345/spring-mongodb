a81d7ee937e087cbbbb1d03f7c140cd72dd00a84
zntongana <zntongana@bitri.co.bw>
2021-06-02 23:58:32 +0200

    First commit

---
+++ src/main/java/com/example/mongodbhealth/MongodbhealthApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodbhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mongodbhealth/MongodbhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodbhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
